1cc451e7314894e3a4f0f86f5af85a7801b34fa5
Bipin Thapa <gredlead1@gmail.com>
2026-01-16 14:57:11 +0545

    Refactor ProgramShortcut properties for consistency
    Updated properties to use auto-implemented properties with default values.

--- main/Classes/ProgramShortcut.cs
+++ main/Classes/ProgramShortcut.cs
@@ -1,20 +1,20 @@
-﻿namespace client.Classes
+namespace client.Classes
 {
     public class ProgramShortcut
     {
-        public string FilePath { get; set; }
+        public string FilePath { get; set; } = "";
         public bool isWindowsApp { get; set; }
 
         public string name { get; set; } = "";
-        public string Arguments = "";
-        public string WorkingDirectory = MainPath.exeString;
-        public string CustomIconPath = "";
- 
 
-        public ProgramShortcut() // needed for XML serialization
-        {
+        public string Arguments { get; set; } = "";
+        public string WorkingDirectory { get; set; } = MainPath.exeString;
 
-        }
+        // Store either:
+        // - relative path under config/<GroupName>/...  OR
+        // - absolute path
+        public string CustomIconPath { get; set; } = "";
 
+        public ProgramShortcut() { }
     }
 }
